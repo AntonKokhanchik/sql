762803396cd8a0e172ee1dd5fb760c2c084570a9
Kokhanchik Anton <kokhanchik.tversu@gmail.com>
2017-02-23 14:38:25 +0300

    some fixes in creation

--- creation.sql
+++ creation.sql
@@ -5,13 +5,15 @@ drop table if exists students;
 
 # создаём таблицы
 create table IF NOT EXISTS students (
-	n_zach integer primary key unique,
+	n_zach integer primary key auto_increment,
 	student_name varchar(32) not null,
 	student_surname varchar(32) not null,
 	student_lastname varchar(32),
-    number_telephone integer,
+    number_telephone varchar(30),
     n_group integer
 ) charset = utf8;
+
+alter table students auto_increment = 17000;
 
 create table IF NOT EXISTS teachers (
 	n_subject integer primary key auto_increment,
@@ -29,17 +31,17 @@ create table IF NOT EXISTS students_subjects (
 )charset = utf8;
 
 # заполняем таблицы
-insert into students(n_zach, student_name, student_surname, student_lastname, number_telephone, n_group) values 
-	(7141, "Гюннар", "Йоунассон", "Стейнар",940977, 34),
-    (8950, "Бальдюр", "Йоунссон", "Кристьян",363348, 44),
-    (6986, "Бальдюр", "Торкельссон", "Синдри", 489389, 25),
-	(3136, "Торстейдн", "Гримссон", "Йоунас", 515226, 45),
-    (5211, "Сверрир", "Фридрикссон", "Бьёдн", 576194, 14),
-    (7584, "Ингибьёрг", "Тоумасдоуттир", "Сигюрбьёрг", 621932, 15),
-    (4906, "Ауслёйг", "Торкельсдоуттир", "Эльса", 139227, 44),
-    (8970, "Бьёрг", "Сигфусдоуттир", "Роуса", 728365, 34),
-    (7443, "Тоура", "Йоунасдоуттир", "Сванхильдюр", 438703, 24),
-    (8769, "Сигридюр", "Гвюднадоуттир", "Кристьяна", 194119, 35);
+insert into students(student_name, student_surname, student_lastname, number_telephone, n_group) values 
+	("Гюннар", "Йоунассон", "Стейнар",940977, 34),
+    ("Бальдюр", "Йоунссон", "Кристьян",363348, 44),
+    ("Бальдюр", "Торкельссон", "Синдри", 489389, 25),
+	("Торстейдн", "Гримссон", "Йоунас", 515226, 45),
+    ("Сверрир", "Фридрикссон", "Бьёдн", 576194, 14),
+    ("Ингибьёрг", "Тоумасдоуттир", "Сигюрбьёрг", 621932, 15),
+    ("Ауслёйг", "Торкельсдоуттир", "Эльса", 139227, 44),
+    ("Бьёрг", "Сигфусдоуттир", "Роуса", 728365, 34),
+    ("Тоура", "Йоунасдоуттир", "Сванхильдюр", 438703, 24),
+    ("Сигридюр", "Гвюднадоуттир", "Кристьяна", 194119, 35);
 
 insert into teachers(subject_name, teacher_name) values 
 	("Скандинавские руны", "Любим Кронидович Быховский"),
@@ -49,16 +51,16 @@ insert into teachers(subject_name, teacher_name) values
 	("ОБЖ", "Гера Цезаревна Водова");
 
 insert into students_subjects(n_zach, n_subject, mark) values 
-	(7141, 3, 4),
-    (8950, 4, 3),
-    (6986, 1, 5),
-    (6986, 2, 3),
-	(3136, 4, 2),
-    (5211, 5, 5),
-    (7584, 5, 3),
-    (4906, 4, 5),
-    (8970, 3, 2),
-    (7443, 1, 2),
-    (7443, 2, 4),
-    (8769, 3, 5);
+	(17000, 3, 4),
+    (17001, 4, 3),
+    (17002, 1, 5),
+    (17002, 2, 3),
+	(17003, 4, 2),
+    (17004, 5, 5),
+    (17005, 5, 3),
+    (17006, 4, 5),
+    (17007, 3, 2),
+    (17008, 1, 2),
+    (17008, 2, 4),
+    (17009, 3, 5);
 
